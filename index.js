0eae3dfa019b3202ae1d4aa57d6b2ceb0ba214f7
evgene30 <evgene.fe@gmail.com>
2021-10-08 15:03:53 +0300

    add request

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const notifier = require("mail-notifier");
-const {nanoid} = require('nanoid')
+const { nanoid } = require("nanoid");
+const https = require("https");
+const userMail = "evgene.fe@gmail.com";
+const mailPass = "Evgen206774999";
+const token = "2088050898:AAEhUpYW8covVpjktBSVMFeBbAc8qnsA400";
+const idBot = "98887119";
 
 const imap = {
-    user: "evgene.fe@gmail.com",
-    password: "Evgen206774999",
+    user: userMail,
+    password: mailPass,
     host: "imap.gmail.com",
     port: 993,
     tls: true,
-    tlsOptions: {rejectUnauthorized: false},
+    tlsOptions: { rejectUnauthorized: false },
 };
 
-notifier(imap).on("mail", (mail) => {
-    let mails = new Mail(mail.headers.from, mail.date, mail.text);
-    console.log(mails)
-}).start();
+notifier(imap)
+    .on("mail", (mail) => {
+        let mails = new Mail(mail.headers.from, mail.date, mail.text);
+        sendBotMessage(token, idBot, mails);
+    })
+    .start();
 
+function sendBotMessage(token, id, message) {
+    let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${id}&text=${message.headers}, ${message.date}, ${message.text}`;
+    https.get(url);
+}
 
 class Mail {
     constructor(headers, date, text) {
         this.id = nanoid();
         this.headers = headers;
         this.date = date.toLocaleString();
-        this.text = text.replace(/\r?\n|\r/g, " ").replace(/[-]/g, '').replace(/\s+/g, " ").trim();
+        this.text = text
+            .replace(/\r?\n|\r/g, " ")
+            .replace(/[-]/g, "")
+            .replace(/\s+/g, " ")
+            .trim();
     }
 }
-
-
-
-
