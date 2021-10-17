918d6535ec6362c66e56587ab028bfae2992ca22
evgene30 <evgene.fe@gmail.com>
2021-10-17 23:47:06 +0300

    add response json

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const botControl = require("./bot/bot");
 const startGetMail = require("./getmail/getmail");
 const express = require("express");
 const app = express();
+const DATA = require("./data/db.json")
 
 app.set("port", process.env.PORT || 5000);
 
 
 app.get("/", function (request, response) {
-    const result = "App is running";
+    const data = JSON.stringify(DATA);
     botControl(); // bot control
-    response.send(result);
+    response.send(data);
 }).listen(app.get("port"), function () {
     console.log(
         "App is running, server is listening on port ",
