9639ca990d3d337a72c48a558991d42b434255c6
evgene30 <evgene.fe@gmail.com>
2021-10-17 23:38:03 +0300

    add express

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const botControl = require("./bot/bot");
 const startGetMail = require("./getmail/getmail");
+const express = require("express");
+const app = express();
 
-botControl(); // bot control
+app.set("port", process.env.PORT || 5000);
+
+
+app.get("/", function (request, response) {
+    const result = "App is running";
+    botControl(); // bot control
+    response.send(result);
+}).listen(app.get("port"), function () {
+    console.log(
+        "App is running, server is listening on port ",
+        app.get("port")
+    );
+});
