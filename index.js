f923c1fadd6d1d843a94b7eefd19c4af4efbdf12
evgene30 <evgene.fe@gmail.com>
2021-10-17 19:12:44 +0300

    add server http

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const http = require("http");
+const PORT = 3000;
+const DATA = require("./data/db.json")
 const botControl = require("./bot/bot");
 const startGetMail = require("./getmail/getmail");
 
-botControl(); // bot control
-// startGetMail(); // get mail (env)
+
+const server = http.createServer((req, res) => {
+    console.log('Server request');
+    console.log(req.url, req.method);
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+
+    botControl(); // bot control
+    // startGetMail(); // get mail (env)
+
+
+    const data = JSON.stringify(DATA);
+    res.end(data);
+
+})
+
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`Listening port ${PORT}`)
+})
+
