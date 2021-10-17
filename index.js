3f0792a2899e0591065cd1798a28e0042c379e8d
evgene30 <evgene.fe@gmail.com>
2021-10-17 23:56:37 +0300

    add json server

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const DATA = require("./data/db.json")
 
 app.set("port", process.env.PORT || 5000);
 
-
-app.get("/", function (request, response) {const data = DATA;
+app.get("/", function (request, response) {
     botControl(); // bot control
-    response.send(DATA);
+    response.send(DATA); // response JSON
 }).listen(app.get("port"), function () {
     console.log(
         "App is running, server is listening on port ",
