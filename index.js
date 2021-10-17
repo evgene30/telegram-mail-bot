4bc916515889c99730fdff740403dab079487330
evgene30 <evgene.fe@gmail.com>
2021-10-17 23:21:06 +0300

    add rw file json

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const http = require("http");
-const DATA = require("./data/db.json");
-const PORT = 3000;
 const botControl = require("./bot/bot");
 const startGetMail = require("./getmail/getmail");
 
-const server = http.createServer((req, res) => {
-    console.log("Server request");
-    console.log(req.url, req.method);
-    res.setHeader("Content-Type", "application/json; charset=utf-8");
-
-    const data = JSON.stringify(DATA);
-    res.end(data);
-});
-
-// botControl(); // bot control
-
-server.listen(PORT, "localhost", (error) => {
-    error ? console.log(error) : console.log(`Listening port ${PORT}`);
-});
+botControl(); // bot control
