1f9067ca5e74d176797aa681b768cd669b0db847
evgene30 <evgene.fe@gmail.com>
2021-10-17 19:29:01 +0300

    add new port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
-const PORT = 3000;
-const DATA = require("./data/db.json")
+const DATA = require("./data/db.json");
+const PORT = 32500;
 const botControl = require("./bot/bot");
 const startGetMail = require("./getmail/getmail");
 
@@ -10,9 +10,8 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
     res.setHeader("Content-Type", "application/json; charset=utf-8");
 
-
-
-
+    botControl(); // bot control
+    // startGetMail(); // get mail (env)
 
     const data = JSON.stringify(DATA);
     res.end(data);
@@ -21,7 +20,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`Listening port ${PORT}`)
-    botControl(); // bot control
-    // startGetMail(); // get mail (env)
+
 })
 
